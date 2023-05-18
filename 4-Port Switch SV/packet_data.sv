df74f6079b04cb154f22d5c65be8baffd0f92f1f
unlim-int-soumya <srd30312@gmail.com>
2023-05-18 14:07:18 -0400

    Modified

--- 4-Port Switch SV/packet_data.sv
+++ 4-Port Switch SV/packet_data.sv
@@ -1,13 +1,3 @@
-/*-----------------------------------------------------------------
-File name     : packet_data.sv
-Developers    : Brian Dickinson
-Created       : 01/08/19
-Description   : lab1 packet data item 
-Notes         : From the Cadence "Essential SystemVerilog for UVM" training
--------------------------------------------------------------------
-Copyright Cadence Design Systems (c)2019
------------------------------------------------------------------*/
-
 // Follow instructions in lab book
 
 // add print and type policies here
